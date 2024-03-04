1cfda42eff2966ea2dac6ce53303b6162b3f6dc9
kepler452b123 <adept5535@gmail.com>
2024-03-03 16:51:58 -0800

    Drawing functionality, clear and resize grid Added event listeners to each square to turn background color to black if mouse is down on the square. Added clear button which calls a function that goes through each square and resets the background color. Added resize button which clears the grid and then generates a new grid with the given dimensions. Made it so grid has the same width and height, and centered all elements. Removed square borders to give a more blank feel.

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 const grid= document.querySelector(".grid");
-for (let i = 0; i < 16; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 16; j++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+const promptButton = document.querySelector("#prompt");
+const clearButton = document.querySelector("#clear");
+
+let dimensions = 16
+generateGrid(dimensions);
+
+function generateGrid(dimension){
+    for (let i = 0; i < dimension; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < dimension; j++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener('mouseover', function (e) {
+                if (e.buttons === 1){
+                    e.target.style.background = "black";
+                }
+            })
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
 }
+
+function clearGrid(){
+    const listRows = grid.getElementsByClassName("row");
+    for (row of listRows){
+        const listSquares = row.getElementsByClassName("square");
+        for (square of listSquares){
+            square.style.background = "white";
+        }
+    }
+}
+
+promptButton.addEventListener('click', () => {
+    dimensions = parseInt(prompt("Enter dimensions for the grid"));
+    clearGrid();
+    generateGrid(dimensions);
+})
+
+clearButton.addEventListener('click', () =>{
+    clearGrid();
+})
+
+
+
